1a30e4f4ad7ee8b3b5ed472cc6b6efa5efc47674
park <acd2283@gmail.com>
2025-03-19 01:30:54 +0900

    feat nginx 설정 및 dockerfile 수정

--- src/api/api.js
+++ src/api/api.js
@@ -33,7 +33,7 @@ api.interceptors.response.use(
         const refreshAuthorization = Cookies.get("refresh_authorization");
 
         const response = await axios.post(
-          "http://localhost:8080/reToken",
+          "http://3.39.130.212:8080/reToken",
           {},
           {
             // 헤더에 담아서 보냄
